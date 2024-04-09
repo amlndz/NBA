1f23a1a6c0a5bb98073dedd140b0cdd130d38fc5
amlndz <alejandromelendez2014@gmail.com>
2024-04-09 22:18:17 +0200

    [48] - Añadir fotos de las pistas en avif

--- Front-Office/teamInfo.php
+++ Front-Office/teamInfo.php
@@ -179,7 +179,7 @@
     <div class="container-fluid page-header mb-5 position-relative overlay-bottom">
         <div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style="min-height: 400px">
             <h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase"><?php echo $full_name." (".$abbreviation.")"?></h1>
-            <img src="./assets/img/teams/<?php echo $id; ?>.svg" alt="imagen logo equipo" width=170rem>        
+            <img src="./assets/img/teams/<?php echo $id; ?>.svg" alt="imagen logo equipo" width=200rem>        
         </div>
     </div>
     <!-- Page Header End -->
@@ -205,7 +205,7 @@
     </div>
     <div class="container teams-stats-graphs">
         <div class="container-fluid py-8 d-flex justify-content-center"> <!-- Añade flexbox para centrar -->
-            <img src=<?php echo "./assets/img/court/".$id.".jpeg" ?> alt="" width="600px" height="300px">
+            <img src=<?php echo "./assets/img/court/".$id.".avif" ?> alt="" width="600px" height="300px">
         </div>
     </div>
     <!-- Graficas con estadisticas de los equipos -->
